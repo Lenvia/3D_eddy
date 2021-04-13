0a0756b267746ceda4933d7aa3a60bee02707814
Lenvia <yrp1199@163.com>
2021-04-14 00:54:16 +0800

    refactor: 检测更新

--- matlab/generateImages_circ.m
+++ matlab/generateImages_circ.m
@@ -7,14 +7,14 @@ ensembleNo = 1;
 metricThreshold = 100;
 normThreshold = 1000;
 
-dataFile = sprintf('circ%d.nc',ensembleNo);
+dataFile = sprintf('3dAttr%d.nc',ensembleNo);
 surfaces = logical(zeros(500,500,60));
 
-for timestep = 1:30
-    bed = ncread(dataFile, 'circ', [1,1,timestep], [inf,inf,1]);
+for timestep = 1:5
+    bed = ncread(dataFile, 'circ', [1,1,1,timestep], [inf,inf,1,1]);
     bed = fliplr(single(bed));  % fliplr  ×óÓÒ·­×ª¾ØÕó 
     bed = bed';
-    clims = [0 3500];
+	clims = [-2 2];
     gcf = imagesc(bed, clims);
     colorbar
     saveas(gcf, sprintf('/Users/yy/Documents/MATLAB/circ%d/circ%dTimestep%d.png',ensembleNo,ensembleNo,timestep));
