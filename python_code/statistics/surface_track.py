5866279c921ff3bd83650867767c559072eaa679
Lenvia <yrp1199@163.com>
2021-04-26 21:40:27 +0800

    feat: 优化追踪算法（降低阈值，使用重叠法进行补救）

--- python_code/statistics/surface_track.py
+++ python_code/statistics/surface_track.py
@@ -29,7 +29,8 @@ graph2 = []
 
 root = '../result/eddyInfo'
 
-threshold = 0.65
+threshold = 0.3
+showhold = 3
 
 bound = 60
 
@@ -85,21 +86,22 @@ def wirte_features():
                     circ = json_data['circ']
                     vort = json_data['vort']
                     eke = json_data['eke']
+                    aveeke = json_data['aveeke']
 
-                frameFile = os.path.join(path, name+'.mat')
-                frame = scio.loadmat(frameFile)
-                frame = frame['eddie_mask']  # 三维数组
+                # frameFile = os.path.join(path, name+'.mat')
+                # frame = scio.loadmat(frameFile)
+                # frame = frame['eddie_mask']  # 三维数组
 
                 k = 0
-                while k < frame.shape[2]:
-                    if np.max(frame[:, :, k]) > 0:
-                        k += 1
-                    else:
-                        break
-                # print(k-1)
+                # while k < frame.shape[2]:
+                #     if np.max(frame[:, :, k]) > 0:
+                #         k += 1
+                #     else:
+                #         break
+
 
                 # 位置索引, 直径（半径），动能，深度, 涡度，极性，
-                auxf.append([x, y, r, eke, k, vort, circ])
+                auxf.append([x, y, r, eke, aveeke, vort, circ])
                 emptyList.append([])
                 emptyList2.append([])
 
@@ -119,6 +121,9 @@ def track():
         print("day:", i)
         temp = 0
         for j in range(len(graph[i])):  # len(graph[i])表示第i天涡旋的个数
+
+            name1 = str(i)+'-'+str(j)
+
             # graph[i][j] 表示第i天第j个涡旋的延续数组
             flag = false
             # 还有下一天
@@ -126,14 +131,14 @@ def track():
                 curr = np.array(features[i][j])
 
                 for k in range(len(graph[i + 1])):  # 对于第i+1天的每个涡旋
+
+                    name2 = str(i+1)+'-'+str(k)
+
                     nxt = np.array(features[i + 1][k])
 
-                    # 满足某些条件，可认为k是当前涡旋j的延续
-                    # if i == 0 and j == 5:
-                    #     print("------------")
-                    curD = compute(curr, nxt)
+                    curD = compute(name1, name2, curr, nxt)
                     if curD < threshold:
-                        print(str(i)+'-'+str(j), '->', str(i+1)+'-'+str(k))
+                        # print(str(i)+'-'+str(j), '->', str(i+1)+'-'+str(k))
                         graph[i][j].append(str(i+1) + '-' + str(k))
                         graph2[i+1][k].append(str(i) + '-' + str(j))  # 当前涡旋j是下一天涡旋k的前身
 
@@ -141,16 +146,19 @@ def track():
 
                 if len(graph[i][j]) == 0 and i+2 < len(graph):  # 没找到后继， 找下一天
                     for k in range(len(graph[i + 2])):  # 对于第i+2天的每个涡旋
+
+                        name2 = str(i + 2) + '-' + str(k)
                         nxt = np.array(features[i + 2][k])
 
                         # if i == 0 and j == 5:
                         #     print("------------")
 
                         # 满足某些条件，可认为k是当前涡旋j的延续
-                        curD = compute(curr, nxt)
+                        curD = compute(name1, name2, curr, nxt)
                         # print(curD)
+                        # print(str(i) + '-' + str(j), '->', str(i + 2) + '-' + str(k))
                         if curD < threshold:
-                            print(str(i)+'-'+str(j), '->', str(i+2)+'-'+str(k))
+                            # print(str(i)+'-'+str(j), '->', str(i+2)+'-'+str(k))
                             graph[i][j].append(str(i + 2) + '-' + str(k))
                             graph2[i+2][k].append(str(i) + '-' + str(j))  # 当前涡旋j是下一天涡旋k的前身
 
@@ -159,16 +167,18 @@ def track():
                     # 还没有后继，继续找
                     if len(graph[i][j]) == 0 and i + 3 < len(graph):  # 没找到后继， 找下一天
                         for k in range(len(graph[i + 3])):  # 对于第i+3天的每个涡旋
+                            name2 = str(i + 3) + '-' + str(k)
+
                             nxt = np.array(features[i + 3][k])
 
-                            # if i == 0 and j == 5:
-                            #     print("------------")
 
                             # 满足某些条件，可认为k是当前涡旋j的延续
-                            curD = compute(curr, nxt)
-                            # print(curD)
+                            curD = compute(name1, name2, curr, nxt)
+
+
+
                             if curD < threshold:
-                                print(str(i)+'-'+str(j), '->', str(i+3)+'-'+str(k))
+                                # print(str(i)+'-'+str(j), '->', str(i+3)+'-'+str(k))
                                 graph[i][j].append(str(i + 3) + '-' + str(k))
                                 graph2[i+3][k].append(str(i) + '-' + str(j))  # 当前涡旋j是下一天涡旋k的前身
 
@@ -177,16 +187,16 @@ def track():
                         # 还没有后继，继续找
                         if len(graph[i][j]) == 0 and i + 4 < len(graph):  # 没找到后继， 找下一天
                             for k in range(len(graph[i + 4])):  # 对于第i+4天的每个涡旋
+
+                                name2 = str(i + 4) + '-' + str(k)
+
                                 nxt = np.array(features[i + 4][k])
 
-                                # if i == 0 and j == 5:
-                                #     print("------------")
-
                                 # 满足某些条件，可认为k是当前涡旋j的延续
-                                curD = compute(curr, nxt)
-                                # print(curD)
+                                curD = compute(name1, name2, curr, nxt)
+
                                 if curD < threshold:
-                                    print(str(i) + '-' + str(j), '->', str(i + 4) + '-' + str(k))
+                                    # print(str(i) + '-' + str(j), '->', str(i + 4) + '-' + str(k))
                                     graph[i][j].append(str(i + 4) + '-' + str(k))
                                     graph2[i + 4][k].append(str(i) + '-' + str(j))  # 当前涡旋j是下一天涡旋k的前身
 
@@ -194,46 +204,75 @@ def track():
             if flag:
                 temp += 1
 
-        print(temp)
+        # print(temp)
 
 
 # 传来两个数组
-def compute(curr, nxt):
+def compute(name1, name2, curr, nxt):
     # 两个涡核的距离
     # print(curr, nxt)
     delta_dis = np.abs(geodistance(lon_arr[int(curr[0])], lat_arr[int(curr[1])], lon_arr[int(nxt[0])], lat_arr[int(nxt[1])]))
     # 两个涡旋半径差
     delta_r = np.abs(curr[2] - nxt[2])
 
-    if delta_dis > 100:  # 相距太远的也过滤掉
+    if delta_dis > 60:  # 相距太远直接return
         return 999999
+    elif delta_dis > 15:  # 还是有点远，但不是没可能，等待补救
+        curD = 999999
 
     # 涡度差
     delta_xi = np.abs(curr[5] - nxt[5])
-
     # eke差
-    delta_eke = np.abs(curr[3] - nxt[3])
-
-    # curD = sqrt(w1 * (delta_dis / d0) ** 2 + w2 * (delta_r / r0) ** 2 + w3 * (delta_xi / xi0) ** 2 + w4 * (
-    #         delta_eke / eke0) ** 2)
+    delta_eke = np.abs(curr[4] - nxt[4])*10000
 
     curD = sqrt(w1 * (delta_dis / d0) ** 2 + w2 * (delta_r / r0) ** 2)
 
-    if curD > threshold and delta_dis < 30:  # 可能有东西合并过来了导致半径突然增大？
-        curD = 0
+    # curD = sqrt(w1 * (delta_dis / d0) ** 2 + w2 * (delta_r / r0) ** 2 + w4 * (delta_eke / eke0) ** 2)
+
+    if curD > threshold:  # 补救方案
+        # if delta_dis < 20:   # 可能有东西合并过来了导致半径突然增大？
+        #     curD = 0
+        if overlap(name1, name2):
+            curD = 0
 
     # if curD < threshold:
-    # if curr[2] > 50:
-    # if threshold < curD < 0.7:
-    #     print([(curr[0]), (curr[1]), (nxt[0]), (nxt[1])])
-    #     print("距离差: ", delta_dis)
-    #     print("半径差: ", delta_r)
-    #     print("涡度差: ", delta_xi)
-    #     print("eke差: ", delta_eke)
-    #
-    #     print(curD)
+    if name1 == '1-15' and name2 == '2-17':
+        print("距离差: ", delta_dis, "差异：", w1 * (delta_dis / d0) ** 2)
+        print("半径差: ", delta_r, "差异：", w2 * (delta_r / r0) ** 2)
+        print("涡度差: ", delta_xi, "差异：", w3 * (delta_xi / xi0) ** 2)
+        print("eke差: ", delta_eke, "差异：", w4 * (delta_eke / eke0) ** 2)
+
+        print(curD)
 
     return curD
+
+
+def overlap(name1, name2):
+    day1 = name1.split('-')[0]
+    day2 = name2.split('-')[0]
+    path1 = os.path.join(root, str(day1), name1)
+    path2 = os.path.join(root, str(day2), name2)
+
+    frameFile1 = os.path.join(path1, name1+'.mat')
+    frame1 = scio.loadmat(frameFile1)
+    frame1 = frame1['eddie_mask'][:,:,0]  # 表层数组
+
+    frameFile2 = os.path.join(path2, name2+'.mat')
+    frame2 = scio.loadmat(frameFile2)
+    frame2 = frame2['eddie_mask'][:,:,0]  # 表层数组
+
+    area1 = np.sum(frame1 > 0)
+    area2 = np.sum(frame2 > 0)
+    over = np.sum(np.multiply(frame1, frame2) >0)
+
+    ratio = 0.6
+
+    if over > ratio*area1 or over > ratio*area2:
+        print("--------------------------")
+        print(name1, name2, area1, area2, over)
+        return true
+    else:
+        return false
 
 
 def plot():
